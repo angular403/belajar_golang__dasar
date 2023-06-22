cf938d4fdb524001c2adcd2e3dcde7a59e975e98
Andrew Napitupulu <angularsprings403@gmail.com>
2023-06-22 16:31:56 +0700

    penambahan function

--- func.go
+++ func.go
@@ -10,6 +10,18 @@ func name(firstName string, lastName string){
 	fmt.Println("Hello", firstName, lastName)
 }
 
+func getHello(name string) string{
+		if name == ""{
+			return "Hello Bro"
+		}else{
+			return "Hello " + name
+		}
+	} 
+
+func getFullName()(string, string){
+	return "Andrew", "Wiliam"
+}
+
 func main() {
 
 	for i := 0; i <= 10; i++ {
@@ -18,4 +30,11 @@ func main() {
 
 	name("Andrew","Wiliam")
 
+	fmt.Println(getHello("Andrew"))
+
+
+	fmt.Println("=====================================")
+
+	fisrtNamed, lastNamed := getFullName()
+	fmt.Println(fisrtNamed, lastNamed)
 }
